271d674a023cc3bcefb22313728a58b68d251aba
Abusaleh Alam Khan <akhan223720@bscse.uiu.ac.bd>
2025-12-27 21:39:39 +0600

    Test

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,7 @@
 console.log(a); // output : undefined
 var a = 10;
 
-console.log(b); // output : reference error
+console.log(b); // output : reference error (TDZ)
 let b = 30;
 
 console.log(c); // output : reference error (only declaration move on top not assigned)
